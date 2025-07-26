1f5fda249a31d3a5a5376e174280ce230d97b863
Sanjay Ramesh P <sramesh@ecomchain.com>
2025-07-26 12:18:01 +0530

    save changes

--- server/models/User.js
+++ server/models/User.js
@@ -26,6 +26,9 @@ const UserSchema = mongoose.Schema({
 }, { timestamps: true });
 
 UserSchema.pre('save', async function (next) {
+    if (this.firstname) this.firstname = this.firstname.trim().replace(/\s+/g, ' ').toUpperCase();
+    if (this.lastname) this.lastname = this.lastname.trim().replace(/\s+/g, ' ').toUpperCase();
+
     if (!this.isModified('password')) return next();
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
